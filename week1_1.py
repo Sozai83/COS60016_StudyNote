7f5ac0c09401bcfe1cd55ff1d1e9337f17752c78
Sozai83 <chikushi3.227@gmail.com>
2023-10-05 20:52:39 +1100

    input and print a variable

--- week1_1.py
+++ week1_1.py
@@ -18,3 +18,20 @@ age = '32'
 
 # print variables
 print (name, surname, age)
+
+
+# Input and print a variable
+
+name = input("What's your name? ")
+
+# Print the output
+print(name)
+
+#Specify a second variable
+surname = input("What's your surname? ")
+
+# print the output
+print(surname)
+
+# print both outputs
+print(f'Hi {name} {surname}, Nice to meet you.')
