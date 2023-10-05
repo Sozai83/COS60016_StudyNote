d0ceffdf7c4682942b1ac32c47c516b4e32b5d3c
Sozai83 <chikushi3.227@gmail.com>
2023-10-05 21:32:49 +1100

    basic operators

---
+++ week1_2.py
@@ -0,0 +1,19 @@
+#Basic operators
+# Create variables.
+small = 10
+big = 20 
+
+# View the output.
+print("small + big = ",small + big)
+print("small - big = ",small - big)
+print("small * big = ",small * big)
+print("small * big = ",small / big)
+
+# Create variables.
+admin_password = 1234  
+user_input = 1235  
+
+# View the output.
+print(admin_password == user_input) 
+print(admin_password <= user_input) 
+print(admin_password != user_input) 
