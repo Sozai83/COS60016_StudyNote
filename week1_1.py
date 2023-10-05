bc886b7dfa4889ef1f49044ef334108005658562
Sozai83 <chikushi3.227@gmail.com>
2023-10-05 20:49:29 +1100

    variabels

---
+++ week1_1.py
@@ -0,0 +1,20 @@
+#Variables
+
+# Create the age variable and set it to equal to 52
+age = 52
+
+# Use the print() function to return the age variable
+print(age)
+
+name = 'Susan'
+
+print(f'{name} is {age} yo.')
+
+# Specify the variables
+
+name = 'Shiori'
+surname = 'Coding'
+age = '32'
+
+# print variables
+print (name, surname, age)
