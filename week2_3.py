d1a074b3fbf2e28ea3754bfcfdc9efcbd93da5de
Sozai83 <chikushi3.227@gmail.com>
2023-10-12 21:19:50 +1100

    loop control statements

--- week2_3.py
+++ week2_3.py
@@ -73,3 +73,45 @@ while i < 3:
     while i2 < 3:
         i2 += 1
         print(f'Count inside: {i2}')
+
+
+
+## Loop control statements
+
+# Create a list.
+todo_list = ["take out trash", "play video games", "cook dinner", "learn programming"]
+
+#
+for task in todo_list:
+    if(task == 'cook dinner'):
+        print(f'stopping because of {task}')
+        break
+    print(task)
+
+# continue
+for task in todo_list:
+    if(task == 'cook dinner'):
+        continue
+    print(task)
+
+
+#The pass statement in Python is used when a statement is required but you do not want to run any command or code. It is often used as a placeholder for future implementation of loops or functions. 
+for task in todo_list:
+    pass
+
+
+# Create a variable.
+big_counter = 0
+
+# Insert while loop.
+while big_counter < 10:
+    big_counter += 1
+    if big_counter == 5:
+        continue
+    print("Loop #", big_counter)
+    counter = 0
+    while counter < 10:
+        counter += 1
+        if counter == 6:
+            break
+        print(counter)
