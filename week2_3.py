0236e340d3d0fe5892021ec8f961370ac6cf34a8
Sozai83 <chikushi3.227@gmail.com>
2023-10-12 20:34:43 +1100

    for loop '

---
+++ week2_3.py
@@ -0,0 +1,23 @@
+# Print each item in the list.
+cities = ['London', 'Paris', 'New York City', 'Singapore', 'Melbourne', 'Cape Town']
+
+# 'item' in this line is an object in the list.
+for city in cities:
+    print(city)
+
+
+a = 'Top Cities'
+for word in a:
+    print(word)
+
+
+tup_a = ('London', 'Paris', 'New York City', 'Singapore', 'Melbourne', 'Cape Town')
+for t in tup_a:
+    print(t)
+
+
+a_dict = {'city': 'London', 'age': 2000, 'population': 8.9}
+for d in a_dict:
+    print(d)
+    # city, age, population
+
