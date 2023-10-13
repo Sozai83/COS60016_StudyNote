7df36fef5a1bdb408e29ed3cfcddec1490ec7a38
Sozai83 <chikushi3.227@gmail.com>
2023-10-13 12:46:59 +1100

    class attribute

--- week2_4.py
+++ week2_4.py
@@ -1,4 +1,5 @@
 class Person:
+    #An instance attribute is a Python variable belonging to only one object. 
     def __init__(self, name, age, language):
         self.name = name
         self.age = age
@@ -13,3 +14,14 @@ person_object.introduce()
 
 person_object2 = Person('Jane', 42, 'German')
 person_object2.introduce()
+
+### Classes Objects and instances
+
+# Code for demonstration.
+# A class attribute is something fixed and unique to each class. 
+class Person2:
+    
+    # Specify parameters.
+    name = 'Karl'
+    age = 22
+
