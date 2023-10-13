e2c0eae89d8cc45518d1085dfb65ea1ed9377565
Sozai83 <chikushi3.227@gmail.com>
2023-10-13 16:04:51 +1100

    instant attributes

--- week2_4.py
+++ week2_4.py
@@ -15,9 +15,7 @@ person_object.introduce()
 person_object2 = Person('Jane', 42, 'German')
 person_object2.introduce()
 
-### Classes Objects and instances
-
-# Code for demonstration.
+### Class Attributes
 # A class attribute is something fixed and unique to each class. 
 class Person2:
     
@@ -25,3 +23,41 @@ class Person2:
     name = 'Karl'
     age = 22
 
+### Instance Attributes
+
+# Returns true (has) if 'age' exists (attr).
+hasattr(person_object, 'age') 
+
+# Returns (get) value of 'age' attribute (attr).
+getattr(person_object, 'age') 
+
+# Sets (set) attribute (attr) 'age' to 19, for example.
+setattr(person_object,'age',19) 
+
+# Deletes (del) the attribute (attr) 'age'.
+delattr (person_object, 'age')
+
+
+x = hasattr(Person2, 'age')
+print(x) #True
+
+x = hasattr(Person2, 'lang')
+print(x) #False
+
+x = getattr(Person2, 'age')
+print(x) #22
+
+# x = getattr(Person2, 'lang')
+# print(x) #AttributeError: type object 'Person2' has no attribute 'lang'
+
+setattr(Person2, 'age', 100)
+x = getattr(Person2, 'age')
+print(x) #100
+
+delattr(Person2, 'age')
+x = hasattr(Person2, 'age')
+print(x) #False
+
+setattr(person_object2, 'self.age', 100)
+x = getattr(person_object2, 'self.age')
+print(x) #100
